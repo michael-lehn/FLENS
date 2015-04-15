da05f0190967e1f689b1eab8f8cfcc8bb19e1e1b
Michael Lehn <michael.lehn@uni-ulm.de>
2015-04-15 08:33:15 +0200

    Recovered example code

--- flens/examples/tut01-page01-example4.cc
+++ flens/examples/tut01-page01-example4.cc
@@ -19,7 +19,18 @@ main()
 
     cout << "A = " << A << endl;
 
+    ///
+    ///  We want to create a const-view.  However, we do not declare `B` as
+    ///  const.  Ideally the instantiation here would already cause a compile
+    ///  time error.  You see next, why.
+    ///
     DGeMatrixConstView  B = A(_(2,3),_);
 
+    ///
+    ///  `GeMatrix` implements two function operators for element access.  For
+    ///  a const-view merely the read-only access must be allowed.  But as `B`
+    ///  is not declared as const the read-write access gets called.
+    ///
     cout << "B(1,1) = " << B(1,1) << endl;
+
 }
